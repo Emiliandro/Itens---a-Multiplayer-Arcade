1a65f181a286a6d0625d25b592d67e7d7dd81b45
Emiliandro Firmino <emiliandro@live.com>
2016-06-02 12:52:10 -0400

    Testes de Cameras

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2D : MonoBehaviour {
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+
+        // Check for Input if not moving
+        if ((Vector2)transform.position == dest)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && valid(Vector2.up))
+                dest = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && valid(Vector2.right))
+                dest = (Vector2)transform.position + Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && valid(-Vector2.up))
+                dest = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && valid(-Vector2.right))
+                dest = (Vector2)transform.position - Vector2.right;
+        }
+    }
+    bool valid(Vector2 dir)
+    {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
