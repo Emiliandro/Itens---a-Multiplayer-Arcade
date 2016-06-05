7ce5abe7309e89223e0164eb338ba45c0abd814a
Emiliandro Firmino <emiliandro@live.com>
2016-06-05 04:02:04 -0400

    2/3 do menus

---
+++ Assets/Scripts/PanelControllers.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PanelControllers : MonoBehaviour
+{
+    public enum cenas
+    {
+        end = 1,
+        start = 2
+    };
+
+    //chamar o enumerador
+    public cenas qual;
+    public GameObject[] scenes;
+    // Use this for initialization
+    void Start()
+    {
+        if (qual == cenas.start)
+        {
+            Time.timeScale = 0;
+            scenes[1].SetActive(false);
+            scenes[2].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (qual == cenas.start)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1;
+                scenes[0].SetActive(false);
+                scenes[1].SetActive(true);
+            }
+            if (qual == cenas.end)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene("Multiplayer");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SetBackgroundSprit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetBackgroundSprit : MonoBehaviour {
+
+    public Sprite[] cavernas;
+    private int Count;
+    public GameObject bg;
+    // Use this for initialization
+	void Start () {
+        Count = Random.Range(0, cavernas.Length);
+
+        bg.GetComponent<SpriteRenderer>().sprite = cavernas[Count];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
